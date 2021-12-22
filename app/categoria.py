a9e88a4d283894e3dd2011b145609c775fb16270
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-12-21 21:23:59 -0600

    comandos para correr el proyecto

---
+++ app/categoria.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'Mensaje': 'Bienvenido'})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
