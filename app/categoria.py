4cc41526f683bb3af5809beac61f2e19d361e6dd
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-12-21 21:40:40 -0600

    configuracion para la BD

--- app/categoria.py
+++ app/categoria.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
 
 # Nombre de mi Aplicacion
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:ico:@localhost/bd_flask'
 
+# Mensaje de Bienvenida
 @app.route('/', methods=['GET'])
 def in dex():
     return jsonify({'Mensaje': 'Bienvenido al Tutorial API'})
